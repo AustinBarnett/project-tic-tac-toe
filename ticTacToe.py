c48fe9a4493ac656355027d609a3cc7cf6e26db3
Austin Barnett <abarnett10@mail.valenciacollege.edu>
2016-03-02 23:13:39 -0500

    Made tic tac toe game
    struggled

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,43 +1,42 @@
 def printBoard(board):
-    # TO DO #################################################################
-    # Write code in this function that prints the game board.               #
-    # The code in this function should only print, the user should NOT      #
-    # interact with this function in any way.                               #
-    #                                                                       #
-    # Hint: you can follow the same process that was done in the textbook.  #
-    #########################################################################
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-----')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-----')
+    print (board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
+    
+     
 def checkWinner(board, player):    
     print('Checking if ' + player + ' is a winner...')
-    
-    # TO DO #################################################################
-    # Write code in this function that checks the tic-tac-toe board          #
-    # to determine if the player stored in variable 'player' currently      #
-    # has a winning position on the board.                                  #
-    # This function should return True if the player specified in           #
-    # variable 'player' has won. The function should return False           #
-    # if the player in the variable 'player' has not won.                   #
-    #########################################################################
-    
-    
+    return ((board['top-L'] == player and board['top-M'] == player and board['top-R'] == player) or #across the top
+    (board['mid-L'] == player and board['mid-M'] == player and board['mid-R'] == player) or #across the middle
+    (board['low-L'] == player and board['low-M'] == player and board['low-R'] == player) or #across the bottom
+    (board['top-L'] == player and board['mid-L'] == player and board['low-L'] == player) or #down left side
+    (board['top-M'] == player and board['mid-M'] == player and board['low-M'] == player) or #down the middle
+    (board['top-R'] == player and board['mid-R'] == player and board['low-R'] == player) or #down the right side
+    (board['top-L'] == player and board['mid-M'] == player and board['low-R'] == player) or #diagoal
+    (board['top-R'] == player and board['mid-M'] == player and board['low-L'] == player)) #diagonal
+
+      
 def startGame(startingPlayer, board):
     # TO DO #################################################################
     # Add comments to each line in this function to describe what           #
     # is happening. You do not need to modify any of the Python code        #
     #########################################################################
 
-    turn = startingPlayer
-    for i in range(9):
-        printBoard(board)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = input()
-        board[move] = turn
-        if( checkWinner(board, 'X') ):
-            print('X wins!')
-            break
-        elif ( checkWinner(board, 'O') ):
-            print('O wins!')
-            break
+    turn = startingPlayer #Choosing who goes
+    for i in range(9): #Available spaces
+        printBoard(board) #Generating the board
+        print('Turn for ' + turn + '. Move on which space?') #Asking the player what space they want to choose
+        move = input() #Choose move
+        board[move] = turn #Move happens
+        if( checkWinner(board, 'X') ): #Checking if move wins
+            print('X wins!') #If move wins for x then the win is printed
+            break #End game if won
+        elif ( checkWinner(board, 'O') ):#Check to see if 0 wins
+            print('O wins!') #If o wins then print win sign
+            break #End game if won and if not then game continues
     
         if turn == 'X':
             turn = 'O'
